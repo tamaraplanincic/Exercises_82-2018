3b35214f168d909e6faedd3a0e368051073daec3
DESKTOP-03ED60J\PC <tamaraplanincic7@gmail.com>
2020-11-21 18:13:34 +0100

    Commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/ExerciseResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class ExerciseResult
+    {
+        public ExerciseResult(int iD, string name, string index, int points)
+        {
+            ID = iD;
+            Name = name;
+            Index = index;
+            Points = points;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Index { get; set; }
+        public int Points { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {ID} Name: {Name} Index: {Index} Points: {Points}";
+        }
+    }
+    
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			string constring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			using (SqlConnection con = new SqlConnection(constring))
+            {
+				con.Open();
+				string commandText = "SELECT * FROM ExerciseResults";
+				SqlCommand com = new SqlCommand(commandText, con);
+				SqlDataReader dr = com.ExecuteReader();
+				while (dr.Read())
+                {
+					listBox1.Items.Add(new ExerciseResult(dr.GetInt32(0), dr.GetString(1), dr.GetString(2), dr.GetInt32(3)).ToString());
+                }
+            }
+		}
+    }
+}
